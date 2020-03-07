e51c269f91c868749639610f1ee35042c5825bac
nileshm06 <nilesh.mantena06@gmail.com>
2020-03-07 18:34:38 +0530

    2nd Java program

---
+++ HelloWorlds.java
@@ -0,0 +1,7 @@
+  public class HelloWorlds {        
+       public static void main(String[] args) {         
+    // Prints "Hello, Worlds" to the terminal window.         
+       System.out.println("Hello, Worlds");      
+} 
+
+}
